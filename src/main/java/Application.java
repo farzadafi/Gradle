457e609e221e36c469821ff739b8901ac87d2861
farzadafi <farzadafi40@gmail.com>
2022-08-20 11:20:31 +0430

    write test in main method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,19 @@
+import com.google.gson.Gson;
+import model.User;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class Application {
+    public static void main(String[] args) throws JSONException {
+        Gson gson = new Gson();
+        JSONObject jsonObject = new JSONObject();
+
+        jsonObject.put("firstName","farzad");
+        jsonObject.put("userName","farzadAfi");
+        jsonObject.put("password","afi");
+
+        User user = gson.fromJson(String.valueOf(jsonObject),User.class);
+
+        System.out.println(user);
+    }
 }
