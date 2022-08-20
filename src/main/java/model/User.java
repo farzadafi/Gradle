69668db1e7f8d209aaa1531d38d5d4bd058acae0
farzadafi <farzadafi40@gmail.com>
2022-08-20 10:08:14 +0430

    add some field and lombok annotation

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,13 @@
 package model;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+    private String firstName;
+    private String userName;
+    private String password;
 }
